3bf8bb810de8ce64d4ee6dbc63a83e0c76aef1e8
Anthony Poon <ypoon196@gmail.com>
2018-11-26 23:25:38 +0800

    No isTraded needed

--- src/Controller/Core/StoreItemAPIController.php
+++ src/Controller/Core/StoreItemAPIController.php
@@ -153,7 +153,6 @@ class StoreItemAPIController extends Controller{
         $storeItem->setName($json["name"]);
         $storeItem->setDescription($json["description"] ?? null);
         $storeItem->setWeChatId($json["weChatId"] ?? null);
-        $storeItem->setIsTraded($json["isTraded"] == true);
         $em = $this->getDoctrine()->getManager();
         $em->persist($storeItem);
         $em->flush();
