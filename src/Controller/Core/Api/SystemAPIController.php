dc90d67c4b8a3c46a04cd2495f8ba5d22979b4f1
Anthony Poon <ypoon196@gmail.com>
2018-11-30 23:41:02 +0800

    Add notification API

---
+++ src/Controller/Core/Api/SystemAPIController.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ypoon
+ * Date: 30/11/2018
+ * Time: 11:32 PM
+ */
+
+namespace App\Controller\Core\Api;
+
+use App\Entity\Core\GlobalValue;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class SystemAPIController extends Controller {
+
+    /**
+     * @Route("/api/system/notifications")
+     */
+    public function getNotification() {
+        $repo = $this->getDoctrine()->getRepository(GlobalValue::class);
+        $keys = [
+            "globalNotification",
+            "moduleNotification",
+            "storeFrontNotification",
+            "storeItemNotification"
+        ];
+        $rtn = [];
+        foreach ($keys as $key) {
+            $gv = $repo->findOneBy([
+                "key" => $key
+            ]);
+            $rtn[$key] = $gv->getValue();
+        }
+        return new JsonResponse($rtn);
+    }
+}
